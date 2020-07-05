76fd8ff683d14e3d6d0a7c5cc8c92063dc319f5c
juhS <sgjuhyeon@gmail.com>
2020-07-05 19:26:39 +0900

    200705 수정

--- src/study/변수/studyMain.java
+++ src/study/변수/studyMain.java
@@ -3,7 +3,7 @@ package study.º¯¼ö;
 public class studyMain {
 
 	public static void main(String[] args) {
-		study_01 a = new study_01();
+		study01 a = new study01();
 		a.example1();
 	}
 
--- src/study/연산자/studyMain.java
+++ src/study/연산자/studyMain.java
@@ -3,7 +3,7 @@ package study.¿¬»êÀÚ;
 public class studyMain {
 
 	public static void main(String[] args) {
-		study_02 a = new study_02();
+		study02 a = new study02();
 		a.example1();
 
 	}
